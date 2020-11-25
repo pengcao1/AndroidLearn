142945d8b83966d06832b9037feabbe3fb827183
Peng.Cao <pcao@thoughtworks.com>
2020-11-25 14:56:58 +0800

    Refactor: ScrollView to RecycleView

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/TextItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {
+}
